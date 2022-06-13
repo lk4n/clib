51e96ccc21d2414e7962a0d3765dac622b3f2615
Lex K <lex.kachan@gmail.com>
2022-06-13 12:35:17 +0300

    Init

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = cfg => {
+    const dev = cfg.env === 'development';
+
+    return {
+        syntax: 'postcss-scss',
+        map: dev ? { inline: true } : false,
+        plugins: [
+            require('precss')({
+                lookup: { disable: true },
+                properties: {
+                    // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
+                    disable: true,
+                    preserve: true,
+                },
+            }),
+            require('postcss-import'),
+            require('postcss-nested'),
+            require('autoprefixer'),
+            dev ? null : require('cssnano')()
+        ],
+    }
+}
