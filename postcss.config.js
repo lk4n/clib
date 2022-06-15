77a08e969e5f5ab37b4595402bed03eafa1f2417
Lex K <lex.kachan@gmail.com>
2022-06-15 12:00:13 +0300

    Tools: simplified PostCSS plugin setup
    Disabled PreCSS
    Added plugin 'postcss-advanced-variables'
    Added plugin 'postcss-at-rules-variables'

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,26 @@
-module.exports = cfg => {
-    const prod = cfg.env === 'production';
+module.exports = (cfg) => {
+  const prod = cfg.env === 'production';
 
-    return {
-        syntax: 'postcss-scss',
-        map: prod ? false : { inline: true },
-        plugins: [
-            require('precss')({
-                lookup: { disable: true },
-                properties: {
-                    // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
-                    disable: true,
-                    preserve: true,
-                },
-            }),
-            require('postcss-import'),
-            require('postcss-nested'),
-            require('postcss-sort-media-queries'),
-            require('postcss-strip-inline-comments'),
-            require('autoprefixer'),
-            prod ? require('cssnano') : null
-        ],
-    }
-}
+  return {
+    syntax: 'postcss-scss',
+    map: prod ? false : { inline: true },
+    plugins: [
+      //   require('precss')({
+      //     lookup: { disable: true },
+      //     properties: {
+      //       // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
+      //       disable: true,
+      //       preserve: true,
+      //     },
+      //   }),
+      require('postcss-advanced-variables'),
+      require('postcss-at-rules-variables'),
+      require('postcss-import'),
+      require('postcss-nested'),
+      require('postcss-sort-media-queries'),
+      require('postcss-strip-inline-comments'),
+      require('autoprefixer'),
+      prod ? require('cssnano') : null,
+    ],
+  };
+};
