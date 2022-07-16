dacd5964540ad21aa273ba4ae0b8fcb9a6ad0430
Lex K <lex.kachan@gmail.com>
2022-07-16 05:02:33 +0300

    Update html and js
    There are 2 sections: .main and .modal. Section .main aria-hidden if .modal shown.
    Initial focus set to .modal__title

--- 05-lection3/02-modal/index.js
+++ 05-lection3/02-modal/index.js
@@ -2,6 +2,9 @@
   const close = document.getElementById('modal-close');
   const modal = document.getElementById('modal');
   const modalOpen = document.getElementById('modal-open');
+  // aria magic
+  const main = document.querySelector('.main');
+  const title = document.querySelector('.modal__title');
 
   if (!close || !modalOpen || !modal) {
     return;
@@ -11,15 +14,16 @@
     modal.style.display = 'block';
     // document.body.overflow = 'hidden';       --incorrect
     document.body.style.overflow = 'hidden';
-    //aria magic
-    document.body.setAttribute('aria-hidden', 'true');
+    // aria magic
+    main.setAttribute('aria-hidden', 'true');
+    title.focus();
   });
 
   close.addEventListener('click', () => {
     modal.style.display = 'none';
     // document.body.overflow = 'initial';      --incorrect
     document.body.style.overflow = 'initial';
-    //aria magic
-    document.body.removeAttribute('aria-hidden');
+    // aria magic
+    main.removeAttribute('aria-hidden');
   });
 })();
